946c50cb363d0e4412f9dd9ae7220bd559a56b92
김성재 <seongjae6751@naver.com>
2025-04-02 13:33:59 +0900

    chore: swagger 최신화

--- src/main/java/com/example/riskFinder/controller/CrackApi.java
+++ src/main/java/com/example/riskFinder/controller/CrackApi.java
@@ -1,27 +1,55 @@
 package com.example.riskFinder.controller;
 
-import com.example.riskFinder.dto.WaypointRequest;
+import com.example.riskFinder.dto.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.riskFinder.dto.CrackRequest;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+import java.util.List;
+
 public interface CrackApi {
 
-    @Operation(summary = "크랙 감지 하면 저장", description = "크랙을 탐지하면 저장합니다.")
+    @Operation(summary = "크랙 감지 하면 저장")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "프로젝트 생성 성공"),
-        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-        @ApiResponse(responseCode = "404", description = "팀을 찾을 수 없음")
+        @ApiResponse(responseCode = "201", description = ""),
+        @ApiResponse(responseCode = "400", description = ""),
+        @ApiResponse(responseCode = "404", description = "")
     })
     @PostMapping("waypoint")
     ResponseEntity<Void> saveWaypoint(
             @RequestBody WaypointRequest request
     );
+
+    @Operation(summary = "웨이포인트 날짜 별 목록 및 좌표 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = ""),
+            @ApiResponse(responseCode = "400", description = ""),
+            @ApiResponse(responseCode = "404", description = "")
+    })
+    @GetMapping("/waypoints")
+    ResponseEntity<List<WaypointsResponse>> getWaypoints();
+
+    @Operation(summary = "특정 웨이포인트 별 사진 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = ""),
+            @ApiResponse(responseCode = "400", description = ""),
+            @ApiResponse(responseCode = "404", description = "")
+    })
+    @GetMapping("/waypoints/images")
+    ResponseEntity<List<WaypointImagesResponse>> getWaypointImages();
+
+    @Operation(summary = "날짜별 waypoint에 대한 균열 너비 시계열 데이터 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = ""),
+            @ApiResponse(responseCode = "400", description = ""),
+            @ApiResponse(responseCode = "404", description = "")
+    })
+    @GetMapping("/waypoints/measurements")
+    ResponseEntity<List<WaypointMeasurementsResponse>> getWaypointMeasurements();
 }
