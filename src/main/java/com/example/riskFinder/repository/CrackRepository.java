5c7be0fc24b606927420fdc6ed97dd76eba50fb9
김성재 <seongjae6751@naver.com>
2025-04-02 13:22:02 +0900

    fix: native query로 변경

--- src/main/java/com/example/riskFinder/repository/CrackRepository.java
+++ src/main/java/com/example/riskFinder/repository/CrackRepository.java
@@ -3,12 +3,14 @@ package com.example.riskFinder.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.riskFinder.model.Crack;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CrackRepository extends JpaRepository<Crack, Long> {
 
+    @Query("SELECT MAX(c.detectedAt) FROM Crack c WHERE c.crackId = :crackId")
     LocalDate findLatestDetectionDate(String crackId);
 
     List<Crack> findByCrackId(String crackId);
