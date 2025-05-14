100593dbfccf5138c46cea61adfb78be2e074f3d
seongjae6751 <seongjae6751@naver.com>
2025-05-14 19:19:26 +0900

    feat: crack repository 구현

--- src/main/java/com/example/riskFinder/repository/CrackRepository.java
+++ src/main/java/com/example/riskFinder/repository/CrackRepository.java
@@ -2,10 +2,13 @@ package com.example.riskFinder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.riskFinder.dto.BuildingCrackCountDto;
+import com.example.riskFinder.dto.BuildingCrackWidthDto;
 import com.example.riskFinder.model.Crack;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 public interface CrackRepository extends JpaRepository<Crack, Long> {
@@ -14,4 +17,22 @@ public interface CrackRepository extends JpaRepository<Crack, Long> {
     LocalDate findLatestDetectionDate(String crackId);
 
     List<Crack> findByCrackId(String crackId);
+
+    @Query("""
+    SELECT w.building.id AS buildingId, COUNT(c.id) AS crackCount
+    FROM Crack c
+    JOIN Waypoint w ON c.crackId = w.crackId
+    GROUP BY w.building.id
+    ORDER BY crackCount DESC
+""")
+    List<BuildingCrackCountDto> getCrackCountPerBuilding();
+
+    @Query("""
+    SELECT w.building.id AS buildingId, MAX(cm.widthMm) AS maxWidth
+    FROM CrackMeasurement cm
+    JOIN Waypoint w ON cm.crackId = w.crackId
+    GROUP BY w.building.id
+    ORDER BY maxWidth DESC
+""")
+    List<BuildingCrackWidthDto> getMaxCrackWidthPerBuilding();
 }
