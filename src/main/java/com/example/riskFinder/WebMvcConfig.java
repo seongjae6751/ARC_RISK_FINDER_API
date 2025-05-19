746bbca620423a60bcb3afb75c441fbef4f9dcea
seongjae6751 <seongjae6751@naver.com>
2025-05-19 22:05:43 +0900

    fix: cors 에러 수정

--- src/main/java/com/example/riskFinder/WebMvcConfig.java
+++ src/main/java/com/example/riskFinder/WebMvcConfig.java
@@ -1,11 +1,7 @@
-package com.example.riskFinder.config;
-
-import java.util.List;
+package com.example.riskFinder;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
